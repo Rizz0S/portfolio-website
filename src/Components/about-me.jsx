60563425080c4efddd96783ecfa927f018e11da0
Summer Rizzo <srainrizzo@gmail.com>
2020-08-18 20:12:27 -0400

    I am employed now! yay! (updating relevant info)

--- src/Components/about-me.jsx
+++ src/Components/about-me.jsx
@@ -10,8 +10,8 @@ export default function AboutMe (props) {
             <h2 className={styles.subhead}>My name is Summer Rain Rizzo.</h2>
             <img src={self} className={styles['self-img']} alt="a picture of myself that is totally not a selfie"></img>
             <div className={styles.content}>
-            <p>I've always had an affinity for creativity. Seeing the visions that float around my mind come to life is pure magic. I've worn the hat of a filmmaker, photographer, radio DJ, barista and most recently, a developer. Although the context & execution varies, my drive for each of them comes from the same place in my heart - to make things that make a difference. <span className={styles.noMatter}>(no matter how small)</span> </p>
-            <p>I'm a firm advocate of diversity, inclusion, and accessibility. A few other interests of mine include listening to music, film photography, making various things from scratch, eating apples, and enthusing about my interests.  And, if I know and like you well enough, I'm known to mix a pretty good cocktail.</p>
+            <p>I've always had an affinity for creativity. Seeing the visions that float around my mind come to life is pure magic. I've worn the hat of a developer, filmmaker, photographer, radio DJ, and a barista. Although the context & execution varies, my drive for each of them comes from the same place in my heart - to make things that make a difference. <span className={styles.noMatter}>(no matter how small)</span> </p>
+            <p>Currently, I work as a software engineer at Bloomberg LP. I'm a firm advocate of diversity, inclusion, and accessibility. A few other interests of mine include listening to music, film photography, making various things from scratch, eating apples, and enthusing about my interests.  And, if I know and like you well enough, I'm known to mix a pretty good cocktail.</p>
             <p>I'll leave the rest of getting to know me to you.</p>
             <p>♡</p>
             </div>
